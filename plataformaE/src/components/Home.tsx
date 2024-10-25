abae72b5ab1a6f46f051419fe447b0f1b4672172
Ever Essaú <uselessmexicano@gmail.com>
2024-10-25 12:04:26 -0600

    Home

---
+++ plataformaE/src/components/Home.tsx
@@ -0,0 +1,18 @@
+import "./Home.style.css"
+
+const Home = () => {
+    return <>
+    <article className="article-header">
+        <header>
+            <h1>React: Simple CRUD</h1>
+        </header>
+    </article>
+        <section>
+            <div>This is content part</div>
+        </section>
+
+    </>
+};
+
+export default Home;
+
