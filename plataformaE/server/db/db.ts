602ff6036178c9fd64610ced71f3ed6acf145d06
Ever Essaú <uselessmexicano@gmail.com>
2024-10-29 14:08:08 -0600

    Consultas
    Ya es posible crear consultas basicas (SELECT) , necesito poder insertarlas, modificarlas y borrarlas.

--- plataformaE/server/db/db.ts
+++ plataformaE/server/db/db.ts
@@ -1,16 +1,9 @@
-import { DB } from "../deps";
-import { config } from "../deps";
 import { createClient } from "https://esm.sh/@libsql/client@0.6.0/web";
 
-const env = config();
-const db = new DB(Deno.env || "turso_database.db");
-
-export default db;
-
-console.log(Deno.env.get("TURSO_DATABASE_URL"));
-console.log(Deno.env.get("TURSO_AUTH_TOKEN"));
 
 export const turso = createClient({
-    url: Deno.env.get("TURSO_DATABASE_URL"),
-    authToken: Deno.env.get("TURSO_AUTH_TOKEN"),
+    url: "libsql://plataformamoodle-evermako.turso.io",
+    authToken:"eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzAxMzA4MjIsImlkIjoiODBhMjZmMDEtMTVmYS00NTE4LTk4MmMtMzM4MWNkYzVhMzI2In0.MBl7nk9cMChjyle_yuzw6ZNyr9gaZXPu9DY-GOI4QCiGOgEn79eSQZ1yI0TUl8P9045SCXCjOhGPEqZUzLFTAw"
 });
+
+console.log(await turso.execute("SELECT * FROM usuario"));
